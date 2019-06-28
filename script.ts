594e56db473ec4acd8ce7bd4d3ddc4ca4b036fbf
Grzegorz Tomicki <gtomicki@gmail.com>
2019-06-28 13:13:03 +0200

    update

---
+++ script.ts
@@ -0,0 +1,21 @@
+const ul = document.querySelector('.list');
+const array = ['a', 'b', 'c', 'd', 'e'];
+
+array.forEach(item => {
+  console.log(item);
+  ul.innerHTML += `<li>${item}</li>`;
+});
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Greg", lastName: "User" };
+
+const h1 = document.querySelector('h1');
+h1.textContent = greeter(user);
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,33 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    script: './script.js'
+    script: './script.ts'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: './script.js'
   },
-  module: {rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+            'babel-loader',
+            {
+                loader: 'awesome-typescript-loader'
+            }
+        ],
+        exclude: [/node_modules/]
     }
-  ]},
+    ]
+  },
   plugins: [
     new HtmlWebPackPlugin({
       template: './index.html',
-      filename: 'index.html',
-      inject: false
+      filename: 'index.html'
     })
   ]
 };
