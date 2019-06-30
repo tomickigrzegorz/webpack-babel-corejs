bf6f51419d2870e0b1fa7c2dcc7437aa09446ec6
Grzegorz Tomicki <gtomicki@gmail.com>
2019-06-30 21:36:10 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+import style from './style.css';
+
+const ul = document.querySelector('.list');
+const array = ['a', 'b', 'c', 'd', 'e'];
+
+
+array.forEach(item => {
+  ul.innerHTML += `<li class="${style.red}">${item}</li>`;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,56 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
   return {
     devtool: argv.mode === 'production' ? 'none' : 'source-map',
     mode: argv.mode === 'production' ? 'production' : 'development',
     entry: {
-      script: './script.ts'
+      script: './script.js'
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: './script.js'
     },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"]
-    },
     module: {
       rules: [
         {
-          test: /\.ts$/,
+          test: /\.js$/,
           loaders: [
-            'babel-loader',
-            {
-              loader: 'awesome-typescript-loader'
-            }
+            'babel-loader'
           ],
           exclude: [/node_modules/]
-        }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            argv.mode === 'development'
+              ? 'style-loader'
+              : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 2,
+                modules: {
+                  localIdentName: '[sha1:hash:hex:4]',
+                },
+              }
+            }
+          ]
+        },
       ]
     },
     plugins: [
       new HtmlWebPackPlugin({
         template: './index.html',
         filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css',
+        chunkFilename: '[name].[hash].css',
       })
     ]
   }
