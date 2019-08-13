07f29930ea74f4c822d2fbb69242969a9cb2a3b3
Grzegorz Tomicki <gtomicki@gmail.com>
2019-08-13 22:41:43 +0200

    dev/prod template

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    script: './script.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: './script.js'
-  },
-  module: {rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    }
-  ]},
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-      inject: false
-    })
-  ]
-};
+module.exports = (env, argv) => {
+  const ga = argv.mode === 'production' ? './index-google.html' : 'index.html';
+  return {
+    entry: {
+      script: './script.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: './script.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: ga,
+        filename: ga,
+        inject: true
+      })
+    ]
+  }
+}
